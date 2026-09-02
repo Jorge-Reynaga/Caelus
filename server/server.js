0eb0b6797035512f5eed635dd64bb181053918df
Jorge-Reynaga <dev@jorgereynaga.com>
2026-09-02 14:09:27 -0600

    feat: Add deleteFile function

--- server/server.js
+++ server/server.js
@@ -8,4 +8,4 @@ const server = http.createServer(app);
 
 server.listen(port, () => {
     console.log(`Server running on ${port}`);
-})
+});
--- server/services/file.service.js
+++ server/services/file.service.js
@@ -9,9 +9,27 @@ async function createFile(filePath) {
         logger.filesystem(`Created file at: ${filePath}`);
     } catch (error) {
         if (error.code === "EEXIST") {
-            logger.error(`File already exists at: ${filePath}`)
+            logger.error(`File already exists at: ${filePath}`);
         } else {
-            logger.error(`Couldn't create file at: ${filePath}`)
+            logger.error(`Couldn't create file at: ${filePath}`);
+        }
+    }
+}
+
+async function deleteFile(filePath) {
+    try {
+        await fs.unlink(filePath);
+        logger.filesystem(`Deleted file at: ${filePath}`);
+    } catch (error) {
+        switch (error.code) {
+            case "ENOENT":
+                logger.error(`No such file at: ${filePath}`);
+                break;
+            case "EISDIR":
+                logger.error(`The specified file is a directory at: ${filePath}`);
+                break;
+            default:
+                logger.error(`Couldn't delete file at: ${filePath}`);
         }
     }
 }
