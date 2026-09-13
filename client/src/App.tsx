6d053d4274685a9a18033d45df1fd0c60ec7e67e
Jorge-Reynaga <dev@jorgereynaga.com>
2026-09-13 12:13:07 -0600

    feat: Add MainPanel component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import Sidebar from './components/Sidebar'
+import MainPanel from './components/MainPanel'
 
 import './index.css'
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Sidebar></Sidebar>
+      <MainPanel></MainPanel>
     </>
   )
 }
---
+++ client/src/components/MainPanel.tsx
@@ -0,0 +1,8 @@
+function MainPanel() {
+  return (
+    <div className="h-dvh bg-[#3F3F3F]">
+    </div>
+  )
+}
+
+export default MainPanel
